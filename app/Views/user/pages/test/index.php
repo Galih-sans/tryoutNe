5eb35315ef9a3e958ec7de13ae940a00b0a10afc
Thoriqor024 <thoriqor@gmail.com>
2022-08-22 14:49:36 +0700

    update bug system test pages

--- app/Views/user/pages/test/index.php
+++ app/Views/user/pages/test/index.php
@@ -338,7 +338,6 @@
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="<?= base_url('assets-user/js/dashboard.min.js?v=2.0.4')?>"></script>
-  <script src="<?= base_url('assets-user/js/test.app.min.js')?>"></script>
 </body>
 
 </html>
