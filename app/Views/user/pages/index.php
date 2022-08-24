1f2aafecccfed1e5ea33b96d9fa3c290a1b602df
Thoriqor024 <thoriqor@gmail.com>
2022-08-25 03:20:55 +0700

    update bug register

--- app/Views/user/pages/index.php
+++ app/Views/user/pages/index.php
@@ -8,8 +8,11 @@
 	<!-- Font-->
 	<link rel="stylesheet" type="text/css" href="<?= base_url('assets-user/css/opensans-font.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?= base_url('assets-user/css/material-design-iconic-font.min.css')?>">
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 	<!-- Main Style Css -->
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css'>
     <link rel="stylesheet" href="<?= base_url ('assets-user/css/style.css')?>"/>
+	<link rel="stylesheet" href="<?= base_url ('css/bootstrap.min.css')?>"/>
 </head>
 <body>
 	<div class="page-content">
@@ -30,81 +33,47 @@
 									<p>Silahkan lengkapi informasi berikut untuk melanjutkan proses selanjutnya.  </p>
 								</div>
 								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Nama Lengkap</legend>
+									<div class="mb-3 col-12">										
 											<input type="text" class="form-control" id="Nama Lengkap" name="Nama Lengkap" placeholder="Nama Lengkap" required>
-										</fieldset>
-									</div>									
+									</div>										
 								</div>                                
-								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Email Pengguna</legend>
-											<input type="text" name="Email Pengguna" id="email" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="example@email.com" required>
-										</fieldset>
-									</div>
-								</div>
-								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Nomor Telepon</legend>
-											<input type="text" class="form-control" id="phone" name="phone" placeholder="+62 888-999-7777" required>
-										</fieldset>
+								<div class="form-row justify-content-between">
+									<div class="mb-3">										
+											<input type="text" name="Email Pengguna" id="email" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Email" required>										
+									</div>								
+									<div class="col-6">
+											<input type="text" class="form-control" id="phone" name="phone" placeholder="No Handphone" required>
+										
 									</div>
 								</div>
                                 <div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Password</legend>
-											<input type="text" name="password" id="password" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="" required>
-										</fieldset>
+								<div class="mb-3 col-12">
+									<input type="text" name="password" id="password" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Password" required>
 									</div>
 								</div>
                                 <div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Konfirmasi Password</legend>
-											<input type="text" name="password" id="password" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="" required>
-										</fieldset>
+									<div class="mb-3 col-12">
+											<input type="text" name="password" id="password" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Konfirmasi Password" required>
 									</div>
 								</div>
-								<div class="form-row form-row-date">
-									<div class="form-holder">
-									<fieldset>
-											<legend>Tempat Lahir</legend>
-											<input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control" pattern="text" placeholder="" required>
-										</fieldset>
-										<label class="special-label">Tanggal Lahir</label>
-										<select name="month" id="month">
-											<option value="MM" disabled selected>MM</option>
-											<option value="16">16</option>
-											<option value="17">17</option>
-											<option value="18">18</option>
-											<option value="19">19</option>
-										</select>
-										<select name="date" id="date">
-											<option value="DD" disabled selected>DD</option>
-											<option value="Feb">Feb</option>
-											<option value="Mar">Mar</option>
-											<option value="Apr">Apr</option>
-											<option value="May">May</option>
-										</select>
-										<select name="year" id="year">
-											<option value="YYYY" disabled selected>YYYY</option>
-											<option value="2017">2017</option>
-											<option value="2016">2016</option>
-											<option value="2015">2015</option>
-											<option value="2014">2014</option>
-											<option value="2013">2013</option>
-										</select>
+								<div class="form-row justify-content-between">
+									<div class="mb-3">
+											<input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control" pattern="text" placeholder="Tempat Lahir" required>										
+									</div>
+									<div class="col-6">
+										<div class="input-group date" id="datepicker">
+											<input type="text" class="form-control" id="date"/>        <span class="input-group-append">
+												<span class="input-group-text bg-light d-block">
+													<i class="fa fa-calendar"></i>
+												</span>
+											</span>
+										</div>
 									</div>
 								</div>
 								<div class=" text-center pt-0 px-lg-2 px-1">
                                     <p class="mb-4 text-sm mx-auto">
-                                        Don't have an account?
-                                        <a href="<?=route_to('register')?>" class="text-primary text-gradient font-weight-bold">Sign
-                                            up</a>
+                                        Sudah memiliki akun ?
+                                        <a href="<?=route_to('login')?>" class="text-primary text-gradient font-weight-bold">Login</a>
                                     </p>
                                 </div>								
 							</div>
@@ -121,27 +90,18 @@
 									<p>Silahkan lengkapi informasi berikut untuk melanjutkan proses selanjutnya.  </p>
 								</div>
 								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Nama Lengkap</legend>
+								<div class="mb-3 col-12">
 											<input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap" required>
-										</fieldset>
 									</div>									
 								</div>                                
 								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Email Wali</legend>
-											<input type="text" name="email_wali" id="email-wali" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="example@email.com" required>
-										</fieldset>
+									<div class="mb-3 col-12">
+											<input type="text" name="email_wali" id="email-wali" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Email Wali" required>
 									</div>
 								</div>
 								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Nomor Telepon</legend>
-											<input type="text" class="form-control" id="phone" name="phone" placeholder="+62 888-999-7777" required>
-										</fieldset>
+								<div class="mb-3 col-12">
+											<input type="text" class="form-control" id="phone" name="phone" placeholder="Nomor Handphone" required>
 									</div>
 								</div>											
 							</div>
@@ -157,51 +117,37 @@
 									<h3 class="heading">Data Sekolah</h3>
 									<p>Silahkan lengkapi data berikut untuk melanjutkan proses selanjutnya.  </p>
 								</div>								
-								<div class="form-row">
-									<div class="form-holder">
-										<fieldset>
-											<legend>Provinsi</legend>
+								<div class="form-row justify-content-between">
+									<div class="mb-3">
 											<input type="text" class="form-control" id="first-name" name="first-name" placeholder="Provinsi" required>
-										</fieldset>
 									</div>
-									<div class="form-holder">
-										<fieldset>
-											<legend>Provinsi Bagian</legend>
+									<div class="mb-3">
 											<input type="text" class="form-control" id="last-name" name="last-name" placeholder="Provinsi Bagian" required>
-										</fieldset>
 									</div>
 								</div>
                                 <div class="form-row">
-									<div class="form-holder form-holder-2">                                        
-										<fieldset>							
-                                            <legend>Kota</legend>				
-											<input type="text" class="form-control" id="nama-sekolah" name="nama-sekolah" placeholder="Kota" required>
-										</fieldset>
+									<div class="mb-3 col-12">				
+											<input type="text" class="form-control" id="kota" name="kota" placeholder="Kota" required>
 									</div>
 								</div>
                                 <div class="form-row">
-									<div class="form-holder form-holder-2">                                        
-										<fieldset>							
-                                            <legend>Nama Sekolah</legend>				
+									<div class="mb-3 col-12">				
 											<input type="text" class="form-control" id="nama-sekolah" name="nama-sekolah" placeholder="Nama Sekolah" required>
-										</fieldset>
 									</div>
 								</div>
                                 <div class="form-row">
 									<div class="form-holder form-holder-2">
-										<label class="special-label">Jenjang</label>
 										<select name="provinsi" id="provinsi">
 											<option value="jenjang" disabled selected>Jenjang</option>
 											<option value="">SD</option>
 											<option value="">SMP</option>
 											<option value="">SMA</option>
 											<option value="">Lainnya</option>
-										</select>																			
+										</select>
 									</div>
 								</div>
-                                <div class="form-row">
+								<div class="form-row">
 									<div class="form-holder form-holder-2">
-										<label class="special-label">Kelas</label>
 										<select name="provinsi" id="provinsi">
 											<option value="kelas" disabled selected>Kelas</option>
 											<option value="">1</option>
@@ -217,16 +163,12 @@
 											<option value="">11</option>
 											<option value="">12</option>
                                             <option value="">Lainnya</option>
-									</select>																	
+									</select>
 									</div>								
-								</div>
-								
-			            </section>
-									
-		        	</div>
-										
-		        </form>	
-				
+								</div>								
+			            </section>									
+		        	</div>										
+		        </form>					
 			</div>
 		</div>				
 	</div>
@@ -234,5 +176,8 @@
 	<script src="<?= base_url('assets-user/js/jquery-3.3.1.min.js')?>"></script>
 	<script src="<?= base_url('assets-user/js/jquery.steps.js')?>"></script>
 	<script src="<?= base_url('assets-user/js/main.js')?>"></script>
+	<script src="<?= base_url('assets-user/js/core/bootstrap.min.js')?>"></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js'></script>
+	<script src="<?= base_url('assets-user/js/script.js')?>"></script>	
 </body>
 </html>
