611c21fc8b1ba5cad35cdf42b59fac164a4a679e
Thoriqor024 <thoriqor@gmail.com>
2022-08-31 15:15:21 +0700

    update bug (done)

--- app/Views/user/partial/navhead.php
+++ app/Views/user/partial/navhead.php
@@ -1,3 +1,22 @@
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="">
+  <link rel="icon" type="image/png" href="<?= base_url('assets/favicon/logo-ne.ico') ?>">
+  <title>
+    Neoedukasi
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="<?= base_url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700') ?>" rel="stylesheet" />
+  <!-- Icons -->
+  <link href="<?=  base_url('assets-user/css/nucleo-icons.css')?>" rel="stylesheet" />
+  <link href="<?= base_url('assets-user/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="'https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="<?= base_url('assets/images/logo/logo1.png') ?>" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="<?= base_url('assets-user/css/dashboard.css?v=2.0.4') ?>" rel="stylesheet" />
+</head>
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
@@ -19,9 +38,10 @@
                 </div>
               </a>
             </li>
+            <li class="nav-item d-flex align-items-center">
           <div class="dropdown ">
-                <span type="button" id="page-header-user-dropdown" data-bs-toggle="dropdown" class="d-none d-sm-inline-block ms-2 text-white font-weight-light">Neodiamon <a class="text-white font-weight-bold">+125</a> </span>
-                <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"></i>
+                <span type="button" id="page-header-user-dropdown" data-bs-toggle="dropdown" class="d-sm-inline-block ms-2 text-white font-weight-light">Neodiamon <a class="text-white font-weight-bold">+125</a> </span>
+                <i class="fa fa-fw fa-angle-down text-white d-sm-inline-block opacity-50 ms-1 mt-1"></i>
               <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0" aria-labelledby="page-header-user-dropdown">                
                 <div class="p-2">                  
                   <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
@@ -36,10 +56,11 @@
                 </div>
               </div>
             </div>
+          </li>
             <li class="nav-item d-flex align-items-center">
             <div class="dropdown ">
-                <span type="button" id="page-header-user-dropdown" data-bs-toggle="dropdown" class="d-none d-sm-inline-block ms-2 text-white font-weight-light">John</span>
-                <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"></i>
+                <span type="button" id="page-header-user-dropdown" data-bs-toggle="dropdown" class="d-sm-inline-block ms-2 text-white font-weight-light">John</span>
+                <i class="fa fa-fw fa-angle-down text-white d-sm-inline-block opacity-50 ms-1 mt-1"></i>
               <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0" aria-labelledby="page-header-user-dropdown">
                 <div class="p-3 text-center">                  
                   <p class="mt-2 mb-0 fw-medium">John Smith</p>                  
